f630e9d04e4df5be2c73358bdfbf47032991cad0
17sixnon <134508728+17sixnon@users.noreply.github.com>
2025-05-24 02:49:09 -0300

    Add files via upload

---
+++ Perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Youtube',
+    'O que é JavaScript' ,
+    'O JavaScript é uma linguagem de programção'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma funçãp',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'cria cartão funciona com js',
+    'será que funciona',
+    'SIM FUNCIONA'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
